c7a281dc2c74844fb291cae88e851846cdaf1324
Adrian Smith <smith.adriane@gmail.com>
2022-10-11 16:21:48 -0700

    Moving cells example to playground.

--- examples/cells/test/com/phronemophobic/desk/cells_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.phronemophobic.desk.cells-test
-  (:require [clojure.test :refer :all]
-            [com.phronemophobic.desk.cells :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
